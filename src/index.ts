b63621b87da96fc89978431a4de4678dc2871ed1
Hracik <alahucky@protonmail.com>
2021-12-03 12:17:21 +0100

    simplified code

--- src/index.ts
+++ src/index.ts
@@ -122,9 +122,7 @@ export function ShortcutButtonsPlugin(config: ShortcutButtonsFlatpickr.Config) {
         function setButtonsAttributes(button: HTMLButtonElement, attributes?: ShortcutButtonsFlatpickr.Attributes) {
             Object.keys(attributes).filter((attribute) => supportedAttributes.has(attribute)).forEach((key) => {
                 if (key === 'class') {
-                    attributes[key].split(' ').forEach((className) => {
-                        button.classList.add(className);
-                    });
+                    button.classList.add(...attributes[key].split(' '));
                     return;
                 }
 
